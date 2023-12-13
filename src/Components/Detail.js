26e96d783035c062756537a4c384afd317b23915
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-13 16:36:44 +0300

    THe components file

---
+++ src/Components/Detail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Style/Detail.css';
+
+const Detail = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <a href="#" className="link login-link">Login</a>
+        <a href="#" className="link signup-link">Create account</a>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
