88eaee13836583f90b468022206f811ad1c80277
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-13 18:31:04 +0300

    THe program growth

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -5,8 +5,17 @@ const Detail = () => {
   return (
     <div className="wrapper">
       <div className="container">
-        <a href="#" className="link login-link">Login</a>
-        <a href="#" className="link signup-link">Create account</a>
+        <h2>Welcome to Masomo Bora</h2>
+        <p>Embark on a transformative learning experience with Masomo Bora!</p>
+        <div className="actions">
+          <a href="#" className="link login-link">Login</a>
+          <a href="#" className="link signup-link">Create account</a>
+        </div>
+        <br />
+       
+        <br />
+        
+        <p className="Sul">from Sulumeti</p>
       </div>
     </div>
   );
