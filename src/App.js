c4b5f1319560159bae52a08a178ff8636d364b95
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-13 19:11:46 +0300

    the app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import Detail from './Components/Detail'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import 'Routes' instead of 'Switch'
 
+import Detail from './Components/Detail';
+import Signup from './Components/Signup';
 
 function App() {
   return (
-    <div className="App">
-
-      
-      <Detail />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes> 
+          <Route path="/" element={<Detail />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
